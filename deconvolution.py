6b626bcc5304632cd68d59aa96820839693d21ef
Tim <81442486+TimEpsilon@users.noreply.github.com>
2022-06-16 17:10:36 +0200

    Adjustement deconvolution, fix divide by zero

--- deconvolution.py
+++ deconvolution.py
@@ -26,7 +26,7 @@ P  = '\033[35m' # purple
 W  = '\033[0m'  # white
 B =  '\033[34m' # blue
 
-# plt.close('all')
+plt.close('all')
 
 def gauss_map(dx,dy,s):
     X,Y = np.indices((dx,dy))
@@ -43,6 +43,10 @@ def save_normalized_copy(psf,n_img):
         Path to the psf.
     n_img : 2D array
         The image we want to deconvolve
+        
+    Returns
+    -------
+    bckgr = 
 
     '''
     # Copy of psf
@@ -82,18 +86,17 @@ def save_pos_copy(path):
         img = hdul[0].data
         hdr = hdul[0].header
         
-        # img[img<0]=0
-        
-        img = np.nan_to_num(img)
+        img_min=0
+        img[img<0] = np.min(img[img>0])
         
         n_hdul = fits.PrimaryHDU(img,header=hdr)
         n_hdul.writeto(path.replace("mean","norm")
                                ,output_verify='silentfix',overwrite=True)
         print(G + "Copy of " + path + W)      
-    return
+    return img_min
 
 
-def deconvolution(img,psf,n=40):
+def deconvolution(img,psf,n=30,img_min=0):
     '''
     Attempts a deconvolution on img using a given psf.
 
@@ -103,17 +106,17 @@ def deconvolution(img,psf,n=40):
         Image to deconvol.
     psf : str
         point spread function.
-    path : str
-        Location to save file
-
+    n : int
+        Number of iterations
     '''
 
     if os.path.exists(img.replace("norm","deconvolution")):
         os.remove(img.replace("norm","deconvolution"))
-    stsdas.analysis.restore.lucy(img, psf, img.replace("norm","deconvolution"), 1, 0, niter=n, limchisq=1e-10,verbose=False)
+    stsdas.analysis.restore.lucy(img, psf, img.replace("norm","deconvolution"), 40, 0,
+                                 niter=n, limchisq=1e-15,verbose=False,background=-img_min)
     return
 
-def get_matching_psf(path):
+def get_matching_psf(path,choice=False):
     '''
     Returns the path to corresponding Aldebaran mean image, if it exists.
 
@@ -138,14 +141,44 @@ def get_matching_psf(path):
                                  path.split('/')[-2] + "/" + 
                                  path.split('/')[-1])] + "Aldebaran/"
     
+    good_list = []
     for t in os.listdir(ald_path):
         filt_path = ald_path + t + "/" + filt
         if os.path.exists(filt_path + "/export"):
             name_list = os.listdir(filt_path + "/export/")
             for name in name_list:
-                if "bad" not in name:
-                    break
-            corr_path = filt_path + "/export/" + name + "/" + name + "_mean.fits"
+                if "bad" not in name and os.path.exists(filt_path + "/export/" + name + "/" + name + "_mean.fits"):
+                    good_list.append(filt_path + "/export/" + name + "/" + name + "_mean.fits")
+           
+    if len(good_list) == 0:
+        return
+    
+    if len(good_list) == 1 or choice:
+        corr_path = good_list[0]
+        print(G + "Path found ! " + corr_path + W)
+        return corr_path
+    
+    fig,ax = plt.subplots(len(good_list))
+    for i in range(len(good_list)):
+        f = good_list[i]
+        
+        # plot image
+        with fits.open(f) as hdul:
+            if len(np.shape(hdul[0].data)) == 3:
+                img = hdul[0].data[0,:,:]
+            else:
+                img = hdul[0].data
+            ax[i].imshow(img,norm=PowerNorm(0.3),cmap="afmhot")
+            ax[i].set_title(str(i) + " : " + f.split('/')[-1])
+    
+    plt.show()
+    
+    # Choose image to use
+    _id = len(good_list)+1
+    while _id > len(good_list):
+        _id = input("Please input Aldebaran id to use : ")
+    
+    corr_path = good_list[_id]
     print(G + "Path found ! " + corr_path + W)
     return corr_path
 
@@ -166,7 +199,7 @@ def apply_deconvolution(path):
         return
     
     # Creating positive img
-    save_pos_copy(path)
+    img_min = save_pos_copy(path)
     
     # Getting img
     with fits.open(path.replace("mean","norm")) as img:
@@ -181,21 +214,23 @@ def apply_deconvolution(path):
         d_psf = psf[0].data
         
     # Saving deconvolution
-    deconvolution(path.replace("mean","norm"),psf_path.replace("mean","norm"))
+    deconvolution(path.replace("mean","norm"),psf_path.replace("mean","norm"),img_min=img_min)
     
     # Getting deconv
-    with fits.open(path.replace("mean", "deconvolution")) as hdul:
+    with fits.open(path.replace("mean", "deconvolution"),mode="update") as hdul:
+        hdul[0].data += img_min
         deconv = hdul[0].data
-    
+        
+        hdul.flush()
     
     reconv = convolve2d(deconv, d_psf,mode='same')
     
     fig,ax = plt.subplots(2,2)
     
-    ax[0,0].imshow(np.nan_to_num(d_img),norm=PowerNorm(0.3),cmap="afmhot",interpolation='bicubic')
+    ax[0,0].imshow(d_img,norm=PowerNorm(0.3),cmap="afmhot",interpolation='bicubic')
     ax[0,0].set_title("Betelgeuse : " + path.split('/')[-2])
-    ax[1,1].imshow(np.nan_to_num(d_psf),norm=PowerNorm(0.3),cmap="afmhot",interpolation='bicubic')
-    ax[1,1].set_title("Aldebaran" + path.split('/')[-4])
+    ax[1,1].imshow(d_psf,norm=PowerNorm(0.3),cmap="afmhot",interpolation='bicubic')
+    ax[1,1].set_title("Aldebaran " + path.split('/')[-4])
     ax[1,0].imshow(deconv,norm=PowerNorm(0.3),cmap="afmhot",interpolation='bicubic')
     ax[1,0].set_title("Deconvolution")
     ax[0,1].imshow(reconv,norm=PowerNorm(0.3),cmap="afmhot",interpolation='bicubic')
@@ -227,8 +262,35 @@ print(B + str(len(clean_list)) + " files to work on" + W)
 
 for i in range(len(clean_list)):
     f = clean_list[i]
-    apply_deconvolution(f+"_mean.fits")
-    print(B + str(i+1) + "/" + str(len(clean_list)) + " \n" + W)
+    
+    # Deconvolution
+    # apply_deconvolution(f+"_mean.fits")
+    # print(B + str(i+1) + "/" + str(len(clean_list)) + " \n" + W)
+    
+    # Plot every image
+    filt = f.split('/')[-4]
+    epoch = f.split('/')[-7]
+    with fits.open(f+"_deconvolution.fits") as hdul:
+        hdr = hdul[0].header
+        img = hdul[0].data
+        
+        fov = hdr['CD2_2']*hdr['NAXIS2']/2
+        fov *= 3600*1000
+        
+        plt.figure()
+        plt.imshow(img,norm=PowerNorm(0.3),cmap="afmhot",origin="lower",
+                   extent=[fov, -fov, -fov, fov], aspect="equal",interpolation='bicubic')
+        plt.colorbar()
+        plt.xlabel("Ascension droite relative (mas)")
+        plt.ylabel("Declinaison relative (mas)")
+        plt.title(epoch + " : " + filt)
+        
+        # Angular diameter
+        star = plt.Circle((-fov/np.shape(img)[0],fov/np.shape(img)[1]), 43.7,color='b',fill=False,alpha=0.5)
+        ax = plt.gca()
+        ax.add_patch(star)
+        
+        plt.draw()
     
 
 print(G + "Temps écoulé : " + str(int(time.time() - start)) + "s" + W)
